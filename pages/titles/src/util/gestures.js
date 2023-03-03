cd3e8a5c298ce7572bbce3eb76aee24c9554d787
rafaelreisramos <rrramos@gmail.com>
2023-03-02 23:07:59 -0300

    challenge 2: detect love-you(rock n roll) gesture

--- pages/titles/src/util/gestures.js
+++ pages/titles/src/util/gestures.js
@@ -2,6 +2,7 @@ const { GestureDescription, Finger, FingerCurl } = window.fp
 
 const ScrollDownGesture = new GestureDescription('scroll-down') // ✊️
 const ScrollUpGesture = new GestureDescription('scroll-up') // 🖐
+const LoveYouGesture = new GestureDescription('love-you') // 🤟
 
 /**
  * Scroll Down
@@ -23,11 +24,28 @@ for (let finger of Finger.all) {
   ScrollUpGesture.addCurl(finger, FingerCurl.NoCurl, 1.0)
 }
 
-const knownGestures = [ScrollDownGesture, ScrollUpGesture]
+/**
+ * Love You
+ * thumb: no curl, accept half curled with a bit lower confidence
+ * index: no curl
+ * pink: no curl, accepted with a bit lower confidence
+ * ring, middle: curled
+ */
+LoveYouGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+LoveYouGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.5)
+LoveYouGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0)
+LoveYouGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, 0.8)
+
+for (let finger of [Finger.Middle, Finger.Ring]) {
+  LoveYouGesture.addCurl(finger, FingerCurl.FullCurl, 1.0)
+}
+
+const knownGestures = [ScrollDownGesture, ScrollUpGesture, LoveYouGesture]
 
 const gestureStrings = {
   'scroll-up': '🖐',
   'scroll-down': '✊️',
+  'love-you': '🤟',
 }
 
 export { knownGestures, gestureStrings }
